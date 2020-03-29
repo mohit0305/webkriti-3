7d3d8c0d51d68a6236496bf390df7de37c51ebdf
Himanshu <hmpawar10@gmail.com>
2020-03-29 20:39:55 +0530

    new changes

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ var n= new Date();
 var y= n.getFullYear();
 var m= n.getMonth();
 var d= n.getDate();
-document.getElementById("todate").innerHTML= "("+d + "/" + m + "/" + y+")";
+document.getElementById("todate").innerHTML= "("+d + "/" + (m+1) + "/" + y+")";
+
